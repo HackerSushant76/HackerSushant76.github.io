fd55a57d69c49f1420c1de1ce2632491fe7556ce
Sushant <sushantsingh76626@gmail.com>
2022-09-18 18:47:03 +0530

    complete

--- src/Sections/GetInTouch.jsx
+++ src/Sections/GetInTouch.jsx
@@ -8,6 +8,10 @@ export function GetInTouch(){
         <div className="hover"><a href="https://github.com/HackerSushant76" target="_blank" ><FiGithub size="2rem" /></a></div>
         <div className="hover"><a href="https://www.linkedin.com/in/sushant-singh-yadav-925b4a19a" target="_blank"><AiFillLinkedin size="2rem" /></a></div>
         </div>
-        <p className="bright-text hover" >sushantsingh76626@gmail.com</p>
+        <p className="s-dark-text hover" >sushantsingh76626@gmail.com</p>
+        <p className="s-dark-text hover" >+91-7300677362</p>
+        <br />
+        <br />
+        <p style={{color:"purple"}}>Designed & built by Sushant Singh Yadav</p>
     </div>
 }
