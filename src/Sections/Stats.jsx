f3d9b996cc65a4d0732489d84ca08e58e7e3499f
Sushant <sushantsingh76626@gmail.com>
2022-09-21 01:10:41 +0530

    github calnedar

--- src/Sections/Stats.jsx
+++ src/Sections/Stats.jsx
@@ -1,11 +1,13 @@
 import styles from "./Stats.module.css";
+import GitHubCalendar from "react-github-calendar"
 export function Stats() {
   return (
     <div className={styles.stats}>
         <h2>Github Contribution Calendar</h2>
         <br />
         <br />
-        <div><img src="github-calendar.png" alt="" /></div>
+        {/* <div><img src="github-calendar.png" alt="" /></div> */}
+        <div ><GitHubCalendar responsive="true" username="HackerSushant76"/></div>
     </div>
   );
 }
