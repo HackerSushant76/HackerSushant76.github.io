0fb2712471e1d12ef39626fde66ce72674de6054
Sushant <sushantsingh76626@gmail.com>
2022-11-15 13:17:38 +0530

    mail and phone link

--- src/Sections/GetInTouch.jsx
+++ src/Sections/GetInTouch.jsx
@@ -8,8 +8,9 @@ export function GetInTouch(){
         <div className="hover"><a href="https://github.com/HackerSushant76" target="_blank" ><FiGithub size="2rem" /></a></div>
         <div className="hover"><a href="https://www.linkedin.com/in/sushant-singh-yadav-925b4a19a" target="_blank"><AiFillLinkedin size="2rem" /></a></div>
         </div>
-        <p className="s-dark-text hover" >sushantsingh76626@gmail.com</p>
-        <p className="s-dark-text hover" >+91-7300677362</p>
+        <a className="s-dark-text hover" href="mailto:sushantsingh76626@gmail.com">sushantsingh76626@gmail.com</a>
+        <br />
+        <a className="s-dark-text hover" href="tel:+917300677362">+91-7300677362</a>
         <br />
         <br />
         <p style={{color:"purple"}}>Designed & built by Sushant Singh Yadav</p>
